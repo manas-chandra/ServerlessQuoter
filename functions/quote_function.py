3793d91dd45ecc45b53293db29d382fad63b3ba4
Manas <manaschandra.kamana@ericsson.com>
2023-09-24 20:57:16 +0200

    Added Quoters package

--- functions/quote_function.py
+++ functions/quote_function.py
@@ -1,9 +1,26 @@
+from quoters import Quote
+import json
+
 def quote_function(event, context):
   print("Quote function invoked!!")
+  query_params = event['queryStringParameters']
+
+  result = None
+
+  match query_params["category"]:
+    case "anime":
+      result = Quote.print_anime_quote()
+    case "tv-series":
+      result = Quote.print_series_quote()
+    case "programming":
+      result = Quote.print_programming_quote()
+    case _:
+      result = Quote.print()
+
   return {
   "statusCode": 200,
   "headers": {
     "Content-Type": "application/json"
   },
-  "body": "{\"message\":\"Hello, world!\"}"
+  "body": json.dumps({result})
   }
